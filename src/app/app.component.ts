f343a7df4f5fd40e6603da284acf4363998ae161
stigottarjensen <stig@nornir.io>
2023-07-21 12:11:23 +0200

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SYNXTestApp';
+  result: any;
+
+  constructor(private http: HttpClient) { }
+
+  buttonClick(): void {
+    this.http.get('http://localhost:3000').subscribe((res:any) => this.result = res);
+  }
 }
