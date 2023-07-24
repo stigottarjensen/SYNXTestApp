29d2537ee348aef87afed14d976118eee2fd1b24
stigottarjensen <stig@nornir.io>
2023-07-24 10:59:26 +0200

    xsw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SYNXTestApp';
   result: any;
+  subject: any;
+  taContent: string = "";
+  comments:any[] = [];
 
   constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.subject = webSocket('ws://localhost:8128');
+    this.subject.subscribe({
+      next: (msg: any) => {
+        console.log('message received: ' + msg.message); 
+        this.comments.push({server:msg.message,client:""});  
+        return;
+      }, 
+      error: (err: any) => console.log(err), 
+      complete: () => console.log('complete') 
+     });
+  }
 
   buttonClick(): void {
     this.http.get('http://localhost:3000').subscribe((res:any) => this.result = res);
   }
+
+
+  wsClick(): void {
+    this.subject.next({message: this.taContent});
+    this.comments.push({server:"",client:this.taContent});
+    this.taContent = '';
+  }
+
+
+  
+
 }
